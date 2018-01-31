6843963fa0c8817f3e525d935536fff9e9249d8e
vuhoangminh <vuhoangminh199@gmail.com>
2018-01-31 14:54:36 +0700

    initial ProPProject Runnerv1

---
+++ DemoRunner/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+	
+	private Vector3 dir;
+
+
+
+	// Use this for initialization
+	void Start () {
+		dir = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			if(dir == Vector3.forward){
+				dir = Vector3.left;
+			} else {
+				dir = Vector3.forward;
+			}
+		}
+
+		float amountToMove = speed * Time.deltaTime;
+
+		transform.Translate(dir * amountToMove);
+	}
+}
---
+++ DemoRunner/Assets/Scripts/TitleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScript : MonoBehaviour {
+
+	public GameObject[] tilePrefabs;
+
+	public GameObject currentTile;
+
+	// Use this for initialization
+	void Start () {
+		//tilePrefabs[0]
+		for(int i = 0 ; i < 10 ; i++){
+			SpawnTile();
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnTile(){
+		int randomIndex = Random.Range(0,2);
+		currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex], currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position,Quaternion.identity);
+	}
+}
